8d0470f4f9ed96f722c97b57437755f0d01cee96
Danielle Marie De Guzman <daniellemariedeguzman@gmail.com>
2023-12-04 17:34:01 +0800

    commit#4

--- app/src/main/java/com/deguzman/daniellemarie/block1/p1/quiz/MainActivity2.kt
+++ app/src/main/java/com/deguzman/daniellemarie/block1/p1/quiz/MainActivity2.kt
@@ -1,11 +1,23 @@
 package com.deguzman.daniellemarie.block1.p1.quiz
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var tvName : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        tvName = findViewById(R.id.tv_name)
+
+        val getIntent = intent.getStringExtra("myName")
+
+        tvName.text = "$getIntent"
+
     }
 }

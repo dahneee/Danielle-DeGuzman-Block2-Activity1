7ce346ff3948a482c281f943955120d7c79e42e1
Danielle Marie De Guzman <118868265+dahneee@users.noreply.github.com>
2023-12-04 17:35:17 +0800

    commit#5

--- app/src/main/java/com/deguzman/daniellemarie/block1/p1/quiz/MainActivity2.kt
+++ app/src/main/java/com/deguzman/daniellemarie/block1/p1/quiz/MainActivity2.kt
@@ -1,6 +1,5 @@
 package com.deguzman.daniellemarie.block1.p1.quiz
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
